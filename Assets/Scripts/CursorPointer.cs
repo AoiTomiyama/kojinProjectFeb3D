26b4c8a9598e89a48f2b7e2330bfd8b191f47629
AoiTomiyama <TadoO.o2209@gmail.com>
2025-02-10 17:11:40 +0900

    [change] 移動周りの調整。

--- Assets/Scripts/CursorPointer.cs
+++ Assets/Scripts/CursorPointer.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class CursorPointer : MonoBehaviour
 {
     [SerializeField] LayerMask mask;
+    [SerializeField] float _raycastMaxDistance;
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
     void Update()
     {
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out var hit, 999, mask))
+        var rayEndPos = ray.origin + ray.direction * _raycastMaxDistance;
+        Debug.DrawLine(ray.origin, rayEndPos, Color.green);
+        if (Physics.Raycast(ray, out var hit, _raycastMaxDistance, mask))
         {
             transform.position = hit.point;
         }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -9,12 +9,11 @@ public class PlayerMove : MonoBehaviour
     Rigidbody _rb;
     Transform _camera;
     [SerializeField] private float _speed;
-    [SerializeField] private float _maxSpeed;
     [SerializeField] private Transform _lookAt;
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        _camera = FindAnyObjectByType<CinemachineVirtualCamera>().transform;
+        _camera = Camera.main.transform;
     }
     void Update()
     {
@@ -41,7 +40,7 @@ public class PlayerMove : MonoBehaviour
         var right = Quaternion.AngleAxis(90, Vector3.up) * forward;
         var h = Input.GetAxisRaw("Horizontal");
         var v = Input.GetAxisRaw("Vertical");
-        _rb.AddForce((forward * v + right * h) * _speed * Time.deltaTime);
-        _rb.velocity = Vector3.ClampMagnitude(_rb.velocity, _maxSpeed);
+        var dir = (forward * v + right * h).normalized;
+        _rb.AddForce(_speed * Time.deltaTime * dir);
     }
 }
